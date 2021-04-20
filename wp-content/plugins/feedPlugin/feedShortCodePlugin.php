4db7e5b68d9389ca12299d9cb95e2360ce26bf81
Rahul <rahul.shial@gmail.com>
2021-04-19 22:00:47 -0600

    part 2 shortcode plugin rss feed complete

--- wp-content/plugins/feedPlugin/feedShortCodePlugin.php
+++ wp-content/plugins/feedPlugin/feedShortCodePlugin.php
@@ -13,13 +13,11 @@ Text Domain: kd_feed
 */
 
 include_once( ABSPATH . WPINC . '/feed.php' );
+?>
+<style><?php include('./styles/style.css'); ?></style>
 
-// Add Shortcode
-function feedShortCode( $attr ) {
-
-  shortcode_atts( array (
-    'nbrOfFeed' => 1
-  ), $attr);
+<?php
+function getFeeds( $count ) {
 
   $feed = fetch_feed( 'https://www.konstructdigital.com/feed/' );
 
@@ -28,53 +26,93 @@ function feedShortCode( $attr ) {
     $feed->set_cache_duration( 3600 );
   };
 
-  $limit = $feed->get_item_quantity( $attr['nbrOfFeed'] ); //specify number of feeds
+  $limit = $feed->get_item_quantity( $count ); //specify number of feeds
   $feedItems = $feed->get_items( 0, $limit ); // create an array of items
 ?>
+<div>
+    <style>
+  /* ::before */
+  .feed-container {
+    display:flex;
+    flex-wrap:wrap;
+    justify-content:space-between;
+    max-width:1200px;
+    margin:auto;
+    padding:0
+  }
 
-  <h3>Number of Feeds = <?php echo $attr['nbrOfFeed'] ?></h3>
+  .feed-item {
+    display: block;
+    width: 33%;
+    margin: 20px 0;
+    background-color: #fff;
+    word-break: break-word;
+    overflow: hidden;
+    box-shadow: 0 11px 15px 0 rgba(0,0,0,.13);
+  }
 
+  h3 {
+    font-family: Verdana; 
+    color: red;
+  }
+    </style>
+  <h3>Number of Feeds = <?php echo $count ?></h3>
+  <div class='feed-container'>
 <?php
   foreach( $feedItems as $item ) {
+    $pubDate = $item->get_date('F j, Y');
+    $title = $item->get_title();
+    $titleDate = $item->get_date('F j, Y @ g:i a');
+    $permaLink = $item->get_permalink();
     $categories = $item->get_category();
     foreach( $categories as $key => $categoryValue ) { 
       if ($key == 'term') {
         $category = $categoryValue;
       };
-    };
-  
+    };  
     $authorArray = $item->get_author();
     foreach($authorArray as $authorKey => $authorValue) { 
       if( $authorKey == 'name' ) {
         $author = $authorValue;
       };
     };
-  
-    $pubDate = $item->get_date('F j, Y @ g:i a');
 ?>
-
-  <div>
-    <a  href="<?php echo $item->get_permalink(); ?>"
-        title="<?php echo $item->get_date('F j, Y @ g:i a'); ?>">
-        <?php echo $item->get_title(); ?>
-    </a>
-  </div>
-  <span> <?php echo "Date: " . $pubDate; ?></span>
-  <span> <?php echo "Author: " . $author; ?></span>
-  <span> <?php echo "Category: " . $category; ?></span>
-  <br></br>
+    <div class='feed-item'>
+      <a class="image-container" href=<?php echo $permaLink ?>>
+          <img width="1230" height="615" src="https://www.konstructdigital.com/wp-content/uploads/2021/03/blog-post-microcopy-banner.png" class="feed-post__image wp-post-image" alt="An image" loading="lazy" srcset="https://www.konstructdigital.com/wp-content/uploads/2021/03/blog-post-microcopy-banner.png 1230w, https://www.konstructdigital.com/wp-content/uploads/2021/03/blog-post-microcopy-banner-300x150.png 300w, https://www.konstructdigital.com/wp-content/uploads/2021/03/blog-post-microcopy-banner-1024x512.png 1024w, https://www.konstructdigital.com/wp-content/uploads/2021/03/blog-post-microcopy-banner-768x384.png 768w" sizes="(max-width: 1230px) 100vw, 1230px">
+      </a>
+      <div class='feed-item-text-container'>
+        <p class='feed-date'>
+          <time class='feed-published'><?php echo $pubDate ?></time>
+        </p>
+        <h3>
+          <a class='feed-link' href=<?php echo $permaLink ?> title=<?php echo $titleDate ?>><?php echo $title ?></a>
+        </h3>
+        <p class='feed-metaData'>
+          <span class='feed-category'><?php echo $category; ?></span>
+          <span class='feed-author'><?php echo " * " . $author; ?></span>
+        </p>
+      </div>
+    </div>
 <?php 
   }; 
 ?>
+  </div>
+</div>
+<?php 
+};
+?>
 
 <?php
+function feedShortCode( $attr ) {
+  shortcode_atts( array (
+    'repeat' => 1
+  ), $attr);
 
-  // return $feedItems;
-  // return $feed;
-  // return str_repeat($feed, $attr['nbrOfFeed']);
-
+  return getFeeds( $attr['repeat'] );
 };
+?>
 
+<?php
 add_shortcode( 'kd_feed', 'feedShortCode' );
-
 ?>
---
+++ wp-content/plugins/feedPlugin/styles/style.php
@@ -0,0 +1,32 @@
+
+.feed-container {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1200px;
+  justify-content: space-between;
+  margin: auto;
+}
+
+.feed-item {
+  display: block;
+  width: 100%;
+  margin: 20px 0;
+  background-color: #fff;
+  word-break: break-word;
+  overflow: hidden;
+  box-shadow: 0 11px 15px 0 rgba(0,0,0,.13);
+}
+
+h3 {
+  font-family: Verdana; 
+  color: red;
+}
+
+.kd-blog-posts{
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:space-between;
+  max-width:1200px;
+  margin:auto;
+  padding:0
+}
